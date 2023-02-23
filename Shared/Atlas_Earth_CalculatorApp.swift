55253a94290d3239ec97de184c6e0e4476a3bb66
Joe Beltran <jab102191@gmail.com>
2023-02-23 12:53:41 -0600

    Initial Commit

---
+++ Shared/Atlas_Earth_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atlas_Earth_CalculatorApp.swift
+//  Shared
+//
+//  Created by Joe Beltran on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Atlas_Earth_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
